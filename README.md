56e10802ba34162040ebf0bc6109ff922a2140c5
Fazilat Jahan <fazilat.jahan07@gmail.com>
2024-06-07 19:33:12 +0500

    Student Management System

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Student-Management-System
+# IT HUB SMS - Student Management System
+
+
+## Overview
+Welcome to the IT HUB Student Management System (SMS). This command-line application allows you to manage student enrollments, course selections, tuition fee payments, and view student statuses and balances. Built using TypeScript, the system leverages Inquirer for interactive prompts and Chalk for styled console output, providing a seamless and user-friendly experience for managing students.
+
+
+## In-Memory Data Storage
+The application maintains an in-memory database to store student information during the session. This approach ensures quick access and modification of data. The data is retained throughout the session and is lost once the application is exited.
+
+
+**To use My Student Management System (SMS) App from the command line, simply run: npx it-hub-student-management-system**
+
+
+## Description
+The IT HUB Student Management System (SMS) is designed to streamline the process of managing student records within an educational institution. This command-line tool helps administrators efficiently handle various tasks, such as enrolling students, assigning courses, processing tuition payments, and checking student statuses and balances.
+
+
+## Key Features:
+
+**Enroll Student:** Register new students by assigning them a unique ID and initializing their account.
+
+**Courses:** Enroll students in multiple courses from a predefined list, updating their records accordingly.
+
+**Pay Tuition Fee:** Facilitate tuition fee payments for the enrolled courses. The system checks for sufficient balance before processing the payment and updates the balance after a successful transaction.
+
+**Student Status:** Provide detailed information about a selected student, including their name, ID, enrolled courses, and current balance.
+
+**View Balance:** Display the current balance of a selected student.
+
+**Exit:** End the session and exit the system gracefully, ensuring all in-memory data is cleared.
+
+
+## Connect with Me
+I'm open to collaboration opportunities and welcome your feedback and suggestions. Connect with me on LinkedIn:
+
+http://www.linkedin.com/in/fazilatjahan
+
+Feel free to reach out if you have any questions, ideas, or just want to appreciate!
+
---
+++ app.ts
@@ -0,0 +1,163 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk"
+
+let students: any[] = []
+let condition = true
+
+
+async function fastAnimate(text: string, delay: number = 10) {
+    for (let char of text) {
+        process.stdout.write(char);
+        await new Promise((resolve) => setTimeout(resolve, delay));
+    }
+}
+
+await fastAnimate(chalk.white.bold(`\n\n<--------------------------Welcome To IT HUB SMS - Student Management System-------------------------->\n\n`));
+
+
+
+
+async function SMS() {
+    while (condition) {
+
+        let options = await inquirer.prompt({
+            name: "opt",
+            type: "list",
+            message: "--------------------",
+            choices: ["Enroll Student", "Courses", "Pay Tution Fee", "Student Status", "View Balance", "Exit"]
+        })
+        if (options.opt == "Enroll Student") {
+            let studentName = await inquirer.prompt({
+                name: "named",
+                type: "input",
+                message: "Enter Student Name To Enroll:"
+            })
+            if (studentName.named == false) {
+                await fastAnimate(chalk.white.bold(`\nPlease Enter Student Name First To Enroll in SMS.\n\n`));
+            } else {
+                let id = Math.floor(Math.random() * 10000) + 10000
+                await fastAnimate(chalk.white.bold(`\nStudent ${studentName.named.charAt(0).toUpperCase() + studentName.named.slice(1).toLowerCase()} Enrolled With ID ${id}\n\n`));
+
+                students.push({ name: studentName.named, id: id, balance: 5000, courses: [] })
+                // console.log(students);
+
+
+            }
+        }
+
+        else if (options.opt == "Courses") {
+            let studentSelection = await inquirer.prompt({
+                name: "sselect",
+                type: "list",
+                message: "Select Student To Get Enroll In Course(s):",
+                choices: students.map((item) => `${item.name.charAt(0).toUpperCase() + item.name.slice(1).toLowerCase()} (${item.id})`)
+            })
+
+            let selectedStudent = students.find((item) => `${item.name.charAt(0).toUpperCase() + item.name.slice(1).toLowerCase()} (${item.id})` === studentSelection.sselect)
+            // console.log(selectedStudent);
+
+
+
+            let coursesy = await inquirer.prompt({
+                name: "selection",
+                type: "checkbox",
+                message: "Select Courses:",
+                choices: ["Generative AI Engineering", "Artifical Intelligence", "Cyber Security", "Internet Of Things"]
+            })
+
+            if (coursesy.selection == false) {
+                await fastAnimate(chalk.white.bold(`\nPlease Select Your Course First To Get Enroll in SMS.\n\n`));
+            } else {
+                selectedStudent.courses = coursesy.selection
+
+                await fastAnimate(chalk.white.bold(`\nStudent ${studentSelection.sselect} Successfully Enrolled In Course(s) In ${coursesy.selection.join(`, `)}\n\n`));
+                // console.log(students);
+            }
+        }
+
+        else if (options.opt == "Pay Tution Fee") {
+            let studentSelection = await inquirer.prompt({
+                name: "sselect",
+                type: "list",
+                message: "Select Student To Pay tution Fee",
+                choices: students.map((item) => `${item.name.charAt(0).toUpperCase() + item.name.slice(1).toLowerCase()} (${item.id})`)
+            })
+
+            let selectedStudent = students.find((item) => `${item.name.charAt(0).toUpperCase() + item.name.slice(1).toLowerCase()} (${item.id})` === studentSelection.sselect)
+
+            let Tution = await inquirer.prompt({
+                name: "fee",
+                type: "list",
+                message: "Select Your Enrolled Course",
+                choices: selectedStudent.courses
+            })
+
+            let fee = 0
+            if (Tution.fee == "Generative AI Engineering") {
+                fee = 800
+            }
+            else if (Tution.fee == "Artifical Intelligence") {
+                fee = 1000
+            }
+            else if (Tution.fee == "Cyber Security") {
+                fee = 700
+            }
+            else if (Tution.fee == "Internet Of Things") {
+                fee = 500
+            }
+
+            console.log(`\nYour Current Month Tution Fees Is ${fee}\n`);
+
+            if (selectedStudent.balance >= fee) {
+                selectedStudent.balance -= fee
+                await fastAnimate(chalk.white.bold(`You've Successfully Paid Your Tution Fee \n\n`))
+                // await fastAnimate(chalk.white.bold(`Your Current Balance Is ${selectedStudent.balance}$\n\n`));
+
+
+            }
+            else {
+                await fastAnimate(chalk.white.bold(`Insufficient Balance! Your Current Balance is ${selectedStudent.balance}$ Please Recharge Your Balance.\n\n`));
+                await fastAnimate(chalk.white.bold(`Please Pay Your Tution Fee Within Due Date, Else You'll Be Charged Up By 100$\n\n`));
+            }
+        }
+
+
+
+        else if (options.opt == "Student Status") {
+            let studentSelection = await inquirer.prompt({
+                name: "sselect",
+                type: "list",
+                message: "Select Student To Get Student Status:",
+                choices: students.map((item) => `${item.name.charAt(0).toUpperCase() + item.name.slice(1).toLowerCase()} (${item.id})`)
+            })
+            let selectedStudent = students.find((item) => `${item.name.charAt(0).toUpperCase() + item.name.slice(1).toLowerCase()} (${item.id})` === studentSelection.sselect)
+
+            await fastAnimate(chalk.white.bold(`\n<------------------------------Student Status--------------------------------->\n\n \n Student Name: ${selectedStudent.name.charAt(0).toUpperCase() + selectedStudent.name.slice(1).toLowerCase()} \n\n Student ID: ${selectedStudent.id} \n\n Student Enrolled Course(s): ${selectedStudent.courses.join(`, `)} \n\n Student Current Balance: ${selectedStudent.balance}$\n\n`));
+
+        }
+
+        else if (options.opt == "View Balance") {
+            let studentSelection = await inquirer.prompt({
+                name: "sselect",
+                type: "list",
+                message: "Select Student To View Student Balance:",
+                choices: students.map((item) => `${item.name.charAt(0).toUpperCase() + item.name.slice(1).toLowerCase()} (${item.id})`)
+            })
+
+            let selectedStudent = students.find((item) => `${item.name.charAt(0).toUpperCase() + item.name.slice(1).toLowerCase()} (${item.id})` === studentSelection.sselect)
+
+            await fastAnimate(chalk.white.bold(`\n ${selectedStudent.name.charAt(0).toUpperCase() + selectedStudent.name.slice(1).toLowerCase()}'s Current Balance Is ${selectedStudent.balance}$ \n\n`));
+        }
+
+        else if (options.opt == "Exit") {
+            condition = false
+            let develporName = chalk.white.underline` Fazilat Jahan `;
+            await fastAnimate(chalk.white.bold(`\n Exiting IT HUB Student Management System.......\n\n\n<----------------------------------Developed By: ${develporName}-------------------------------------->\n\n`));
+        }
+
+    }
+}
+
+SMS()
